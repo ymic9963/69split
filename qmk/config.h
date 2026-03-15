907a7af25078c14ed6c2b995a90e53406c728b0c
ymich9963 <gmichael834@gmail.com>
2026-03-15 23:46:00 +0000

    Figured out listing matrix pins and added LICENSE

---
+++ qmk/config.h
@@ -0,0 +1,21 @@
+/* Copyright 2026 Yiannis Michael (ymich9963)
+
+This program is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+#pragma once
+
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 16
+#define MATRIX_ROW_PINS { B1, B2, B3, C6, B6 }
+// #define MATRIX_COL_PINS { D4, D5, D6, D7, F5, F6, F7 }
