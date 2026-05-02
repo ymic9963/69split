a252e756f0f9547eed62bf14329aa6115fb1af53
ymich9963 <gmichael834@gmail.com>
2026-05-03 00:45:26 +0100

    Continue matrix solution

--- qmk/matrix.c
+++ qmk/matrix.c
@@ -1,34 +1,20 @@
 #include <stdbool.h>
 #include <string.h>
-#include "mcp23018.h"
-#include "atomic_util.h"
-#include "matrix.h"
 #include "gpio.h"
+#include "util.h"
+#include "matrix.h"
+#include "atomic_util.h"
+#include "print.h"
 #include "config.h"
 
-#define IOEXPANDER_COLS 8
-#define IOEXPANDER_ADDR 0
-#define IOEXPANDER_COLS_PORT mcp23018_PORTA
-#define IOEXPANDER_ROWS_PORT mcp23018_PORTB
 #define MATRIX_INPUT_PRESSED_STATE 0
 
-static pin_t row_pins[MATRIX_ROWS_PER_HAND] = MATRIX_ROW_PINS;
-static pin_t col_pins[MATRIX_COLS] = MATRIX_COL_PINS;
+static const pin_t row_pins[MATRIX_ROWS_PER_HAND] = MATRIX_ROW_PINS;
+static const pin_t col_pins[MATRIX_COLS] = MATRIX_COL_PINS;
 
 /* All other initialisations happen in qmk_firmware/quantum/matrix_common.c */
-void matrix_init_custom(void) {
-    /* Initialise IO Expander */
-    mcp23018_init(IOEXPANDER_ADDR);
+void matrix_init_custom(void) { }
 
-    /* Set port A (columns) to all output high */
-    mcp23018_set_config(IOEXPANDER_ADDR, IOEXPANDER_COLS_PORT, ALL_OUTPUT);
-    mcp23018_set_output(IOEXPANDER_ADDR, IOEXPANDER_COLS_PORT, ALL_HIGH);
-
-    /* Set port B (rows) to all input with pull-up enabled */
-    mcp23018_set_config(IOEXPANDER_ADDR, IOEXPANDER_ROWS_PORT, ALL_INPUT);
-}
-
-/* From qmk_firmware/quantum/matrix.c */
 static inline void gpio_atomic_set_pin_output_low(pin_t pin) {
     ATOMIC_BLOCK_FORCEON {
         gpio_set_pin_output(pin);
@@ -36,74 +22,19 @@ static inline void gpio_atomic_set_pin_output_low(pin_t pin) {
     }
 }
 
-/* From qmk_firmware/quantum/matrix.c */
+static inline void gpio_atomic_set_pin_output_high(pin_t pin) {
+    ATOMIC_BLOCK_FORCEON {
+        gpio_set_pin_output(pin);
+        gpio_write_pin_high(pin);
+    }
+}
+
 static inline void gpio_atomic_set_pin_input_high(pin_t pin) {
     ATOMIC_BLOCK_FORCEON {
         gpio_set_pin_input_high(pin);
     }
 }
 
-/* Set a single pin to output and to logic low */
-static bool mcp23018_set_output_pin_low(uint8_t slave_addr, mcp23018_port_t port, uint8_t pin) {
-    uint8_t conf = 0;
-    uint8_t ret = 0;
-
-    if (!mcp23018_read_pins(IOEXPANDER_ADDR, port, &ret)) {
-        return false;
-    }
-    conf = ret | ~(0x00000001 << pin);
-
-    if (!mcp23018_set_config(IOEXPANDER_ADDR, port, conf)) {
-        return false;
-    }
-
-    return mcp23018_set_output(slave_addr, port, conf);
-}
-
-/* From qmk_firmware/quantum/matrix.c */
-static bool select_row(uint8_t row) {
-    pin_t pin = row_pins[row];
-    if (row < IOEXPANDER_COLS) {
-        if (pin != NO_PIN) {
-            gpio_atomic_set_pin_output_low(pin);
-
-            return true;
-        }
-    } else {
-        mcp23018_set_output_pin_low(IOEXPANDER_ADDR, IOEXPANDER_ROWS_PORT, pin);
-
-        return true;
-    }
-
-    return false;
-}
-
-/* Set a single pin to input and with pull-up enabled */
-static bool mcp23018_set_input_pin_high(uint8_t slave_addr, mcp23018_port_t port, uint8_t pin) {
-    uint8_t conf = 0;
-    uint8_t ret = 0;
-
-    if (!mcp23018_read_pins(IOEXPANDER_ADDR, port, &ret)) {
-        return false;
-    }
-    conf = ret | (0x000000001 << pin);
-
-    return mcp23018_set_config(IOEXPANDER_ADDR, IOEXPANDER_COLS_PORT, conf);
-}
-
-/* From qmk_firmware/quantum/matrix.c */
-static void unselect_row(uint8_t row) {
-    pin_t pin = row_pins[row];
-    if (row < IOEXPANDER_COLS) {
-        if (pin != NO_PIN) {
-            gpio_atomic_set_pin_input_high(pin);
-        }
-    } else {
-        mcp23018_set_input_pin_high(IOEXPANDER_ADDR, IOEXPANDER_ROWS_PORT, row);
-    }
-}
-
-/* From qmk_firmware/quantum/matrix.c */
 static inline uint8_t readMatrixPin(pin_t pin) {
     if (pin != NO_PIN) {
         return (gpio_read_pin(pin) == MATRIX_INPUT_PRESSED_STATE) ? 0 : 1;
@@ -112,12 +43,24 @@ static inline uint8_t readMatrixPin(pin_t pin) {
     }
 }
 
-/* Read an individual pin from the IO Expander */
-static inline uint8_t mcp23018_read_pin(uint8_t slave_addr, mcp23018_port_t port, uint8_t pin) {
-    uint8_t ret = 0;
-    mcp23018_read_pins(slave_addr, port, &ret);
+static bool select_row(uint8_t row) {
+    pin_t pin = row_pins[row];
+    if (pin != NO_PIN) {
+        gpio_atomic_set_pin_output_low(pin);
+        return true;
+    }
+    return false;
+}
 
-    return ret & pin;
+static void unselect_row(uint8_t row) {
+    pin_t pin = row_pins[row];
+    if (pin != NO_PIN) {
+#            ifdef MATRIX_UNSELECT_DRIVE_HIGH
+        gpio_atomic_set_pin_output_high(pin);
+#            else
+        gpio_atomic_set_pin_input_high(pin);
+#            endif
+    }
 }
 
 /* From qmk_firmware/quantum/matrix.c */
@@ -134,11 +77,9 @@ void matrix_read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row)
     matrix_row_t row_shifter = MATRIX_ROW_SHIFTER;
     for (uint8_t col_index = 0; col_index < MATRIX_COLS; col_index++, row_shifter <<= 1) {
         uint8_t pin_state = 0;
-        if (col_index < IOEXPANDER_COLS) {
-            pin_state = readMatrixPin(col_pins[col_index]);
-        } else {
-            pin_state = mcp23018_read_pin(IOEXPANDER_ADDR, IOEXPANDER_COLS, col_index); 
-        }
+        pin_state = readMatrixPin(col_pins[col_index]);
+        // uprintf("col_index:");
+        // uprintf("%d\n", col_index);
 
         // Populate the matrix row with the state of the col pin
         current_row_value |= pin_state ? 0 : row_shifter;
@@ -156,16 +97,15 @@ void matrix_read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row)
  * Adapted from qmk_firmware/quantum/matrix.c
  * */
 bool matrix_scan_custom(matrix_row_t raw_matrix[]) {
-    matrix_print();
-    bool matrix_has_changed = false;
     matrix_row_t curr_matrix[MATRIX_ROWS] = {0};
 
-    for (uint8_t current_row = 0; current_row < MATRIX_ROWS; current_row++) {
+    // Set row, read cols
+    for (uint8_t current_row = 0; current_row < MATRIX_ROWS_PER_HAND; current_row++) {
         matrix_read_cols_on_row(curr_matrix, current_row);
     }
 
     bool changed = memcmp(raw_matrix, curr_matrix, sizeof(curr_matrix)) != 0;
     if (changed) memcpy(raw_matrix, curr_matrix, sizeof(curr_matrix));
 
-    return matrix_has_changed;
+    return changed;
 }
